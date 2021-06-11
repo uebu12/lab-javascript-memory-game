f2cd973f54814c64f14f6ed0e1c0b2e296918779
John Doe <gabrifurquim@gmail.com>
2021-06-10 21:55:29 -0300

    not done

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,56 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+    this.shuffleCards();
   }
+  // cardOpen() {
+  //   this.pickedCards.push(this);
+  //   var len = this.pickedCards.length;
+  //   if (len === 2) {
+  //     moveCounter();
+  //     if (this.pickedCards[0].type === this.pickedCards[1].type) {
+  //       matched();
+  //     } else {
+  //       unmatched();
+  //     }
+  //   }
+  // }
 
-  shuffleCards() {
-    // ... write your code here
+  shuffleCards(cards) {
+    var currentIndex = this.cards.length,
+      randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [this.cards[currentIndex], this.cards[randomIndex]] = [
+        this.cards[randomIndex],
+        this.cards[currentIndex]
+      ];
+    }
+
+    return cards;
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
